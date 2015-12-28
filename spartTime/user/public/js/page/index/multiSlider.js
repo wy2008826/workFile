5f9b7f219214f211fdfbdb21fec34ac57da7e016
wy2008826 <760104178@qq.com>
2015-12-28 11:29:40 +0800

    test file

--- spartTime/user/public/js/page/index/multiSlider.js
+++ spartTime/user/public/js/page/index/multiSlider.js
@@ -438,7 +438,7 @@
 						alert($wraper.html());
 						alert($wraper.height());
 					},1000);
-					
+
 				};
 				
 
@@ -511,6 +511,7 @@
 			function move(){
 				var now=new Date()*1;
 				var transform=_getTransform(dom);
+				alert(JSON.stringify(transform));
 				if(now-startTime<=durTime){
 					var disY=y-transform.y;
 					var speed=disY / (durTime-(now-startTime));
